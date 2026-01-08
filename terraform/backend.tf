a6b99f4ea9f98b3c31dfe3e588d647dba4ca3774
Matalobo1 <134148816+Matalobo1@users.noreply.github.com>
2026-01-08 16:24:11 +0100

    Fix typo in S3 bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "joao-tfstate-123"
+    bucket         = "joao-tftstate-123"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
